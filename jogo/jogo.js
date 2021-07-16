cf3bab3f8064c0ac90ed1f0df31d1ce785988403
IcaroFranco <icarofup@gmail.com>
2021-07-16 11:16:28 -0300

    iniciando jogo

---
+++ jogo/jogo.js
@@ -0,0 +1,22 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var score = document.getElementById("score")
+
+function jump() {
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(function () {
+        character.classList.remove("animate");
+    }, 500);
+}
+var checkDead = setInterval(function () {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("Vc perdeu! - Aperte f5 para jogar novamente")
+    }
+}, 10)
+//------------------------------------------------------------
